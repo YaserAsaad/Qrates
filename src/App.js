a4d8e836b5dc65657a172f5b6bf471886799f294
YaserAsaad <79270976+YaserAsaad@users.noreply.github.com>
2023-08-10 07:37:59 +0300

    adding the 2nd part

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import PartOne from "./PartOne";
+import SecondPart from "./SecondPart";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <PartOne />
+      <SecondPart />
+    </div>
+  );
+}
+
+export default App;
